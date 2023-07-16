cd28866da10841deaa600c569f1ceb8e7f686fb8
moon <xmun74@naver.com>
2023-07-16 23:47:12 +0900

    feat: helmet, hpp

--- app.js
+++ app.js
@@ -18,7 +18,19 @@ const app = express();
 passportConfig();
 app.set("port", process.env.PORT || 8080);
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+    })
+  );
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+}
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
