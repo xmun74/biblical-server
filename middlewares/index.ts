4b6b9999a8c949de7253b546053abf99e7dfc889
moon <xmun74@naver.com>
2023-10-06 16:26:40 +0900

    chore: delete localstorage

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -12,12 +12,6 @@ const isNotLoggedIn: RequestHandler = (req, res, next) => {
   if (!req.isAuthenticated()) {
     next();
   } else {
-    const localStorageKey = JSON.parse(
-      localStorage.getItem("isLoggedIn") as string
-    );
-    if (!localStorageKey) {
-      localStorage.setItem("isLoggedIn", JSON.stringify(true));
-    }
     res.status(403).json({ message: "이미 로그인했습니다" });
   }
 };
